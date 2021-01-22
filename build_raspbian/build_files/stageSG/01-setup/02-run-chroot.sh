9d2f77206b20114cb9fe4f65a5efee0f12d03df1
Dale Floer <dalefloer@gmail.com>
2021-01-22 14:05:30 -0800

    Added third /data partition and temp mounts.
    Still need to expand /data to be the full SD card after first boot.
    /data/SGdata and /data/config are bind mounted to /boot/SGdata and
    /boot/uboot respectively for compatibility with existing SG software.
    These should go away at some point as the software is update to use the
    /data mountpoint.

--- build_raspbian/build_files/stageSG/01-setup/02-run-chroot.sh
+++ build_raspbian/build_files/stageSG/01-setup/02-run-chroot.sh
@@ -2,8 +2,13 @@
 
 # Set up data and config directories.
 on_chroot << EOF
+    echo "Creating data directory"
     mkdir -p /data
     mkdir -p /data/config
     mkdir -p /data/SGdata
     touch /data/config/deployment.txt
+
+    echo "Creating deprecated /boot mount points."
+    mkdir -p /boot/uboot
+    mkdir -p /boot/SGdata
 EOF
